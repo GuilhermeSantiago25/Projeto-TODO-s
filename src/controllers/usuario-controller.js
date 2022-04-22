3ff03af64146e511376cb62fee10c5240906f4a3
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-22 10:13:30 -0300

    Adicionando rotas PUT e atualizando a DELETE

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -7,7 +7,7 @@ const usuario = app => {
 
         res.json(bd.usuarios);
         //res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados');
-    })
+    });
     
     //rota por parametro :nome
     app.get('/usuario/:nome', function (req, res) {
@@ -17,7 +17,7 @@ const usuario = app => {
         res.json({
             nome
         });
-    })
+    });
     
     //rota por parametro :email
     app.get('/usuario/:email', (req, res) => {
@@ -27,7 +27,7 @@ const usuario = app => {
                 return `o email encontrado é: ${email}`
             }
         }
-    })
+    });
 
     //Cria um novo usuario atraves da classe Usuario
     app.post('/usuario', function (req, res) {
@@ -37,9 +37,9 @@ const usuario = app => {
         bd.usuarios.push(usuarios);
         res.status(201).json({
             "usuarioCadastrado": body.nome, 
-        })
+        });
         //res.send('Rota POST de usuario ativada: usuario adicionado ao banco de dados');
-    })
+    });
     
     //Exclui um usuario pelo nome
     app.delete('/usuario/:nome', (req,res) => {
@@ -53,9 +53,9 @@ const usuario = app => {
         } else {
             res.json({
                 "mensagem": `Usuário com nome "${nome}" não existe`
-            })
+            });
         }
-    })
+    });
     
     
     //Exclui um usuario pelo email;
@@ -67,13 +67,53 @@ const usuario = app => {
             const usuarioDeletado = bd.usuarios.splice(indexUsuario, 1);
             res.json({
                 "deletado": usuarioDeletado,
-            })
+            });
         } else {
             res.json({
                 "mensagem": `Usuário com email "${email}" não existe`,
-            })
+            });
         }
-    })
+    });
+
+    //ATUALIZA O USUARIO PELO NOME;
+    app.put('/usuario/:nome', (req,res) =>{
+        const nome = req.params.nome;
+        const body = req.body;
+        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === nome));
+
+        if(indexUsuario > -1) {
+            const usuarioAntigo = bd.usuarios[indexUsuario];
+            const usuarioAtualizado = new Usuario(
+                body.nome || usuarioAntigo.nome,
+                body.email || usuarioAntigo.email,
+                body.senha || usuarioAntigo.senha,
+                usuarioAntigo.id
+            );
+            res.json({"Atualizado": usuarioAtualizado});
+        } else {
+            res.json({mensagem: `Usuario com ${nome} não existe.`})
+        }
+    });
+
+    //ATUALIZA O USUARIO PELO EMAIL;
+    app.put('/usuario/:email', (req,res) =>{
+        const email = req.params.email;
+        const body = req.body;
+        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.email === email));
+
+        if(indexUsuario > -1) {
+            const usuarioAntigo = bd.usuarios[indexUsuario];
+            const usuarioAtualizado = new Usuario(
+                body.name || usuarioAntigo.nome,
+                body.email || usuarioAntigo.email,
+                body.senha || usuarioAntigo.senha,
+                usuarioAntigo.id
+            );
+            res.json({"Atualizado": usuarioAtualizado});
+        } else {
+            res.json({mensagem: `Usuario com ${email} não existe.`})
+        }
+    });
 
 }
 
