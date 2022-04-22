d916b19453c11f44e4be73c861e1ac0825cffd72
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-22 16:31:52 -0300

    Deixando os Parametros unicos na pesquisa

--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -6,80 +6,60 @@ const usuario = app => {
     app.get('/usuario', function (req, res) {
 
         res.json(bd.usuarios);
-        //res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados');
     });
     
-    //rota por parametro :nome
-    app.get('/usuario/:nome', function (req, res) {
-        const {
-            nome
-        } = req.params.nome;
-        res.json({
-            nome
-        });
-    });
-    
-    //rota por parametro :email
-    app.get('/usuario/:email', (req, res) => {
-        const email = req.params.email;
-        for (let i = 0; i <= bd.length; i++) {
-            if (bd[i].email == email) {
-                return `o email encontrado é: ${email}`
-            }
+    //PROCURA NO BANCO PELO PARAMETRO
+    app.get('/usuario/:indicador', function (req, res) {
+        const indicador = req.params.indicador;
+        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === indicador || usuario.email === indicador));
+        
+        if (indexUsuario > -1) {
+            
+            res.status(200).json({
+                "Usuario": bd.usuarios,
+            });
+        } else {
+            res.json({
+                "mensagem": `Usuário "${indicador}" não existe`,
+            });
         }
-    });
 
+    });
+    
     //Cria um novo usuario atraves da classe Usuario
     app.post('/usuario', function (req, res) {
         const body = req.body;
-        const usuarios = new Usuario(body.nome, body.email, body.senha)
+        const usuarios = new Usuario(body.nome, body.email, body.senha);
 
         bd.usuarios.push(usuarios);
         res.status(201).json({
             "usuarioCadastrado": body.nome, 
         });
-        //res.send('Rota POST de usuario ativada: usuario adicionado ao banco de dados');
     });
     
-    //Exclui um usuario pelo nome
-    app.delete('/usuario/:nome', (req,res) => {
-        const nome = req.params.nome;
-        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === nome))
+    //EXCLUI UM USUARIO PELO PARAMETRO
+    app.delete('/usuario/:indicador', (req,res) => {
+        const indicador = req.params.indicador;
+        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === indicador || usuario.email === indicador));
+        
         if (indexUsuario > -1) {
             const usuarioDeletado = bd.usuarios.splice(indexUsuario, 1);
-            res.json({
+            res.status(200).json({
                 "deletado": usuarioDeletado,
             });
         } else {
-            res.json({
-                "mensagem": `Usuário com nome "${nome}" não existe`
+            res.status(404).json({
+                "mensagem": `Usuário "${indicador}" não existe`
             });
         }
     });
     
-    
-    //Exclui um usuario pelo email;
-    app.delete('/usuario/:email', (req, res) => {
-        const email = req.params.email;
-        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.email === email));
 
-        if (indexUsuario > -1) {
-            const usuarioDeletado = bd.usuarios.splice(indexUsuario, 1);
-            res.json({
-                "deletado": usuarioDeletado,
-            });
-        } else {
-            res.json({
-                "mensagem": `Usuário com email "${email}" não existe`,
-            });
-        }
-    });
-
-    //ATUALIZA O USUARIO PELO NOME;
-    app.put('/usuario/:nome', (req,res) =>{
-        const nome = req.params.nome;
+    //ATUALIZA O USUARIO PELO PARAMETRO;
+    app.put('/usuario/:indicador', (req,res) =>{
+        const indicador = req.params.indicador;
         const body = req.body;
-        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === nome));
+        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === indicador || usuario.email === indicador));
 
         if(indexUsuario > -1) {
             const usuarioAntigo = bd.usuarios[indexUsuario];
@@ -89,29 +69,9 @@ const usuario = app => {
                 body.senha || usuarioAntigo.senha,
                 usuarioAntigo.id
             );
-            res.json({"Atualizado": usuarioAtualizado});
+            res.status(200).json({"Atualizado": usuarioAtualizado});
         } else {
-            res.json({mensagem: `Usuario com ${nome} não existe.`});
-        }
-    });
-
-    //ATUALIZA O USUARIO PELO EMAIL;
-    app.put('/usuario/:email', (req,res) =>{
-        const email = req.params.email;
-        const body = req.body;
-        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.email === email));
-
-        if(indexUsuario > -1) {
-            const usuarioAntigo = bd.usuarios[indexUsuario];
-            const usuarioAtualizado = new Usuario(
-                body.name || usuarioAntigo.nome,
-                body.email || usuarioAntigo.email,
-                body.senha || usuarioAntigo.senha,
-                usuarioAntigo.id
-            );
-            res.json({"Atualizado": usuarioAtualizado});
-        } else {
-            res.json({mensagem: `Usuario com ${email} não existe.`});
+            res.status(404).json({mensagem: `Usuario com ${nome} não existe.`});
         }
     });
 
