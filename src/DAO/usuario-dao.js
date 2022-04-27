2a74275268c55a6f0a191c3ed50c8b79824f8326
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-04-27 15:50:23 -0300

    Atualizando DAO

--- src/DAO/usuario-dao.js
+++ src/DAO/usuario-dao.js
@@ -1,30 +1,29 @@
 const Usuario = require('../models/usuario-model');
 
 class UsuarioDAO {
-    constructor (bd){
-        this.bd= bd;
+    constructor(bd) {
+        this.bd = bd;
     }
 
-    listarUsuario(){
+    listarUsuario() {
         return new Promise((resolve, reject) => {
-            this.bd.all(`SELECT * FROM USUARIOS`,(err, rows) => {
-                if(err){
-                    reject (err);
+            this.bd.all(`SELECT * FROM USUARIOS`, (err, rows) => {
+                if (err) {
+                    reject(err);
                 } else {
-                    resolve (
+                    resolve(
                         rows
                     );
                 }
+            })
         })
-    })}
+    }
 
-    listarUsuarioParam(){
-        return new Promise ((resolve,reject) =>{
-            this.bd.all(`SELECT * FROM USUARIOS WHERE id = $indicador or name = $indicador or email = $indicador`,{
-                $indicador: indicador
-            },(err, rows) => {
+    listarUsuarioParam(indicador) {
+        return new Promise((resolve, reject) => {
+            this.bd.all(`SELECT * FROM USUARIOS WHERE id = ? or name = ? or email = ?`, [indicador], (err, rows) => {
                 if (err) {
-                    reject (err);
+                    reject(err);
                 } else {
                     resolve({
                         "Banco selecionado": rows
@@ -34,21 +33,45 @@ class UsuarioDAO {
         });
     }
 
-    inserirUsuario(usuarios){ 
-        return new Promise((resolve,reject) =>{
+    inserirUsuario(usuarios) {
+        return new Promise((resolve, reject) => {
             this.bd.run(`INSERT INTO USUARIOS (NOME,EMAIL,SENHA) VALUES (?,?,?)`, [usuarios.nome, usuarios.email, usuarios.senha],
-            (err) => {
-                if (err) {
-                    reject (err)
-                } else {
-                    resolve (
-                      'success'
-                    );
-                }
-            });
+                (err) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(
+                            'success'
+                        );
+                    }
+                });
         });
     }
 
+    atualizarUsuario() {
+        return new Promise((resolve, reject) => {
+            const {
+                nome,
+                email,
+                senha
+            } = Usuario;
+            this.bd.run(`UPDATE USUARIOS
+            SET nome = ?,
+            email=?,
+            senha=?,
+            WHERE id = ?`,
+                [nome, email, senha, id],
+                (err) => {
+                    if (err, rows) {
+                        reject(err)
+                    } else {
+                        resolve({
+                            "Usuário atualizado": rows
+                        });
+                    }
+                });
+        });
+    }
 
 }
 
--- src/controllers/usuario-controller.js
+++ src/controllers/usuario-controller.js
@@ -2,81 +2,50 @@ const UsuarioDAO = require('../DAO/usuario-dao');
 const Usuario = require('../models/usuario-model');
 
 const usuario = (app, bd) => {
-    const usuarioDao = new UsuarioDAO(bd);
-    app.get('/usuario', (req, res) => {
-        usuarioDao.listarUsuario()
-            .then((resposta) => {
-                res.json(resposta);
-            })
-            .catch((err) => {
-                res.json(err);
-            })
-    });
-    
-    app.get('/usuario/:indicador', function (req, res) {
-        const indicador = req.params.indicador;
         const usuarioDao = new UsuarioDAO(bd);
         app.get('/usuario', (req, res) => {
-            usuarioDao.listarUsuarioParam(indicador)
-            .then((resposta) => {
-                res.json(resposta);
+            usuarioDao.listarUsuario()
+                .then((resposta) => {
+                    res.json(resposta);
+                })
+                .catch((err) => {
+                    res.json(err);
+                })
+        });
+
+        app.get('/usuario/:indicador', function (req, res) {
+            const indicador = req.params.indicador;
+            const usuarioDao = new UsuarioDAO(bd);
+            app.get('/usuario', (req, res) => {
+                usuarioDao.listarUsuarioParam(indicador)
+                    .then((resposta) => {
+                        res.json(resposta);
+                    })
+                    .catch((err) => {
+                        res.json(err);
+                    })
             })
-            .catch((err)=>{
-                res.json(err);
-            })
-        })
-    });
+        });
 
-    app.post('/usuario', function (req, res) {
-        const body = req.body;
-        const usuarios = new Usuario(body.nome, body.email, body.senha);
-        const usuarioDao = new UsuarioDAO(bd);
-        usuarioDao.inserirUsuario(usuarios)
-            .then(() => {
-                res.json("Sucess");
-            })
-            .catch((err) => {
-                res.json(err);
-            })
-    });
-    app.delete('/usuario/:indicador', (req, res) => {
-        const indicador = req.params.indicador;
-        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === indicador || usuario.email === indicador));
+        app.post('/usuario', function (req, res) {
+            const body = req.body;
+            const usuarios = new Usuario(body.nome, body.email, body.senha);
+            const usuarioDao = new UsuarioDAO(bd);
+            usuarioDao.inserirUsuario(usuarios)
+                .then(() => {
+                    res.json("Sucess");
+                })
+                .catch((err) => {
+                    res.json(err);
+                })
+        });
 
-        if (indexUsuario > -1) {
-            const usuarioDeletado = bd.usuarios.splice(indexUsuario, 1);
-            res.status(200).json({
-                "deletado": usuarioDeletado,
-            });
-        } else {
-            res.status(404).json({
-                "mensagem": `Usuário "${indicador}" não existe`
-            });
-        }
-    });
-    app.put('/usuario/:indicador', (req, res) => {
-        const indicador = req.params.indicador;
-        const body = req.body;
-        const indexUsuario = bd.usuarios.findIndex((usuario => usuario.nome === indicador || usuario.email === indicador));
+        app.delete('/usuario/:indicador', (req, res) => {
 
-        if (indexUsuario > -1) {
-            const usuarioAntigo = bd.usuarios[indexUsuario];
-            const usuarioAtualizado = new Usuario(
-                body.nome || usuarioAntigo.nome,
-                body.email || usuarioAntigo.email,
-                body.senha || usuarioAntigo.senha,
-                usuarioAntigo.id
-            );
-            res.status(200).json({
-                "Atualizado": usuarioAtualizado
-            });
-        } else {
-            res.status(404).json({
-                mensagem: `Usuario com ${nome} não existe.`
-            });
-        }
-    });
+        });
 
-}
+        app.put('/usuario/:indicador', (req, res) => {
 
-module.exports = usuario;
+        });
+    }
+        module.exports = usuario;
